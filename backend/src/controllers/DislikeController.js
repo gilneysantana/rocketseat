ee5e7bdd61632f02a8b4508f0d33fd2dc84d233e
Gilney Dell <gilneysantana@gmail.com>
2020-04-01 12:25:30 -0300

    List funcionando

--- backend/src/controllers/DislikeController.js
+++ backend/src/controllers/DislikeController.js
@@ -12,10 +12,8 @@ module.exports = {
         if (!targetDev)
             return res.status(400).json({error : 'Dev not exists'});
 
-        if (targetDev.likes.includes(loggedDev._id))
-            console.log('Deu match');
 
-        loggedDev.likes.push(targetDev._id);
+        loggedDev.dislikes.push(targetDev._id);
         await loggedDev.save();
 
         return res.json(loggedDev);

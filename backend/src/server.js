7a36f4204a3a908bc0b5a1bfc5a539688e405964
Gilney Dell <gilneysantana@gmail.com>
2020-04-01 12:32:09 -0300

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +9,7 @@ const server = express();
 //mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-gou8m.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser:true});
 mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-gou8m.mongodb.net:27017,cluster0-shard-00-01-gou8m.mongodb.net:27017,cluster0-shard-00-02-gou8m.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser:true});
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);

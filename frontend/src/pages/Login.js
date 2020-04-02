72af0c8ccdd3848dd5695fd7cb4acefcea0eb0ee
Gilney Dell <gilneysantana@gmail.com>
2020-04-02 14:38:57 -0300

    Login.css indo bem

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Login.css';
+import logo from '../assets/gilney.PNG';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="nao achei a figura." />
+        </div>
+    );
+}

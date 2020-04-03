5c5be3d34020887b2a3f944e5a415153a950c4ce
Gilney Dell <gilneysantana@gmail.com>
2020-04-03 10:03:44 -0300

    1:02 ok

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../services/api';
 import logo from '../assets/gilney.PNG';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 import './Main.css';
 
 export default function Main({ match }) {
+
+    //essa funcao sera invocada sempre que o id do usuario logado mudar 
+    useEffect( () => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id,
+                }
+            })
+            console.log(response);
+        }
+
+        loadUsers();
+    }, [match.params.id]);
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
